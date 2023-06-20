2a89841b0b366f845f3d2759830dfb578db6b5a9
slimdreez <rahimacgs@gmail.com>
2023-06-20 05:35:25 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,7 +2,8 @@
 
 /**
  * main - main block
- * Write a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
+ * Write a program that prints all single digit numbers
+ * of base 10 starting from 0, followed by a new line.
  * Return: Always 0 (Success)
  */
 
